7b34b759d2b8bce3db2a965cfa5f2a0c667745bd
toni.paloniemi <toni.paloniemi@admicom.fi>
2024-07-03 19:41:47 +0300

    Generate passphrases from wordlist

--- app/passphrase/actions.ts
+++ app/passphrase/actions.ts
@@ -1,9 +1,36 @@
 "use server";
 
+import {unstable_cache} from 'next/cache';
+
+const passwordFileUrl = process.env.PASSWORD_LIST_URL as string;
+
+async function getPasswordList() {
+  'use server'
+  const response = await fetch(passwordFileUrl)
+  const passwords = await response.text()
+  const passwordList = passwords.split('\n');
+  return passwordList.map(password => password.trim());
+}
+
+const cachedGetWordList = unstable_cache(getPasswordList);
+
+function getRandomWord(passwordList: string[]) {
+  return passwordList[Math.floor(Math.random() * passwordList.length)];
+}
+
 export async function generatePassphrase() {
   'use server'
-  // Generate password from random 8 characters
+  const wordList = await cachedGetWordList();
+  // Generate list of 4 random words from
+  const words = Array.from({length: 4}, () => getRandomWord(wordList));
+  // Generate number between 10 and 999
+  const number = Math.floor(Math.random() * 990) + 10;
+  words.push(number.toString());
+  // Shuffle words list
+  words.sort(() => Math.random() - 0.5);
+  // Generate passphrase from words by separating words with '-'
+  const passphrase = words.join('-');
   return {
-    passphrase: Math.random().toString(36).slice(-8)
+    passphrase
   };
 }
