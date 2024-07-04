c2bf9055129d26dfb554694934f7adc517afc70c
toni.paloniemi <toni.paloniemi@admicom.fi>
2024-07-04 16:50:40 +0300

    Add labdays logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,19 @@ import { DarkThemeToggle, HR } from "flowbite-react";
 import React from "react";
 import { useTranslations } from "next-intl";
 import { AppDetails } from "@/app/app-details";
+import Image from "next/image";
 export default function Home() {
   const t = useTranslations("Home");
   return (
     <main className="flex items-center flex-col pt-4 pb-4 space-y-4 mx-auto max-w-lg px-4">
+      <div className="dark:bg-gray-300 rounded-2xl shadow dark:shadow-gray-300">
+        <Image
+          src="/aitio-labdays2018.png"
+          alt="Labdays logo"
+          width={200}
+          height={128.5}
+        />
+      </div>
       <h1 className="text-2xl">{t("welcome")}</h1>
       <hr></hr>
       <PassphraseComponent />
