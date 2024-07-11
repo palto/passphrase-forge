4f5ce5a62ab74f2e7f534e0337d4f525eb4ef502
toni.paloniemi <toni.paloniemi@admicom.fi>
2024-07-11 18:48:27 +0300

    Replace reading package.json with environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 import createNextIntlPlugin from "next-intl/plugin";
-import pack from './package.json' assert { type: 'json' };
 
 const withNextIntl = createNextIntlPlugin();
-const version = pack.version;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Needs to be here until https://github.com/vercel/next.js/issues/64525 is fixed
   transpilePackages: ['next-mdx-remote'],
   env: {
-    NEXT_PUBLIC_VERSION: version,
+    NEXT_PUBLIC_VERSION: process.env.npm_package_version,
   }
 };
 
