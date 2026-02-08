bda763e7d0a23010bf32609f924f0a8888b5f6a6
Toni Paloniemi <toni.paloniemi@gmail.com>
2026-02-08 10:20:16 +0200

    Set 'environment' to posthog events

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,4 +4,9 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   api_host: "/ph",
   defaults: "2025-11-30",
+  loaded: (ph) => {
+    ph.register({
+      environment: process.env.NEXT_PUBLIC_VERCEL_ENV ?? "development",
+    });
+  },
 });
